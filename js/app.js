7269687c740900439b4de1b3e5eea57061aeb037
kaylamchenry <kayla.mchenry@gmail.com>
2023-11-01 13:22:45 -0700

    firstsavepoint

--- js/app.js
+++ js/app.js
@@ -2,19 +2,68 @@
 
 function calculatePrice(){
     //input value 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let checkInDate = document.getElementById("checkInDate").value;
-    let nights = parseInt(document.getElementById("nights").value;
-    let roomType = document.getElementById("roomType").value;
-    let adults = parseInt(document.getElementById("adults").value
-    let children = parseInt(document.getElementById("children").value
-    let discount = document.getElementById("discount").value;
-
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var checkInDate = document.getElementById("checkInDate").value;
+    var nights = parseInt(document.getElementById("nights").value;
+    var roomType = document.getElementById("roomType").value;
+    var adults = parseInt(document.getElementById("adults").value
+    var children = parseInt(document.getElementById("children").value
+    var discount = document.getElementById("discount").value;
     //calculations with queen, king, 2 bedroom w prices
      let prices = {
         "queen" : {
             "JunAug": 250.00,
-            "RestOfYear"= 150.00
+            "RestOfYear": 150.00,
+        },
+        "king" : {
 
+            "JunAug": 250.00,
+            "RestOfYear": 150.00,
+        },
+        "2 bedroom" : {
+
+            "JunAug": 400.00,
+            "RestOfYear": 300.00,
+        }
+    }
+    function validateForm() {
+        if (isEmpty(document.getElementById('data_2').value.trim())) {
+        alert('First name is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_3').value.trim())) {
+        alert('Last name is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_4').value.trim())) {
+        alert('Phone is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_5').value.trim())) {
+        alert('Email is required!');
+        return false;
+        }
+        if (!validateEmail(document.getElementById('data_5').value.trim())) {
+        alert('Email must be a valid email address!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_6').value.trim())) {
+        alert('Arrival date is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_7').value.trim())) {
+        alert('Departure date is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_8').value.trim())) {
+        alert('Number of adults is required!');
+        return false;
+        }
+        return true;
+        }
+        function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+        function validateEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+        return isEmpty(email) || re.test(email);
         }
